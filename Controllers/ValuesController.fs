c1dd8d6bca0e910b795364fe5a7afad9b7c0aea7
lrakai <lrakai@pyxisinnovation.com>
2016-12-11 23:19:34 -0700

    Initial commit with GET action

---
+++ Controllers/ValuesController.fs
@@ -0,0 +1,13 @@
+﻿namespace FSharpWebApi.Controllers
+ 
+open System
+open System.Collections.Generic
+open Microsoft.AspNetCore.Mvc
+ 
+[<Route("api/Values")>]
+type ValuesController() =
+    inherit Controller()
+ 
+    [<HttpGet>]
+    member this.Get() =
+        ["x"; "z"]
---
+++ Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+open System.IO
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.DependencyInjection
+ 
+type Startup(env: IHostingEnvironment) =
+ 
+    member this.ConfigureServices(services: IServiceCollection) =
+        services.AddMvc() |> ignore
+ 
+    member this.Configure (app: IApplicationBuilder) =
+        app.UseMvc() |> ignore
+ 
+[<EntryPoint>]
+let main argv = 
+    printfn "Starting"
+    let host = WebHostBuilder()
+                .UseUrls("http://0.0.0.0:5000") // bind to any ipv4 address (or run with dotnet run --server.urls=http://0.0.0.0:5000)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build()
+    host.Run()
+    0 //exit code
