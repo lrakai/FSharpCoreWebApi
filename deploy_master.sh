e508b5d391625466816866e5dcad16ad70295576
lrakai <lmrakai@gmail.com>
2016-12-17 21:39:31 -0700

    Fix release push

--- deploy_master.sh
+++ deploy_master.sh
@@ -6,7 +6,6 @@ dotnet publish --output bin/Publish --configuration Release
 
 # Push artifacts to release branch (Triggering docker hub build)
 cd bin/Publish
-git checkout -b "master"
 git config user.name "CircleCI"
 git config user.email "CircleCI@circle.ci"
 git add -f .
